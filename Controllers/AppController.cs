7b0a7d6948d619a198bcf6986ceb77c256706943
Heba Almomani <hebaalmomani1998@gmail.com>
2022-02-03 14:55:42 +0200

    office

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Dutch.wwwroot
+namespace Dutch.Controllers
 {
     public class AppController : Controller
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us ";
+            return View();
+        }
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
             return View();
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,12 @@ namespace Dutch
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+                {
+                app.UseDeveloperExceptionPage();
+                }
+
+
             //app.UseDefaultFiles();
             app.UseStaticFiles();  
 
